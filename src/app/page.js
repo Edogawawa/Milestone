6e3980faacfa846798376cedd7af60704037ae99
Muhammad Atpur Rafif <muhammadatpurrafif@gmail.com>
2023-08-06 21:40:50 +0700

    navbar

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,17 @@
 'use client'
+import "./page.css"
+
 export default function Home() {
   return (
-    <main className="p-3">Test</main>
+    <>
+      <nav className="flex flex-row p-5 gap-3 text-white items-center">
+        <img src="./hourglass.svg" className="w-3"></img>
+        <div className="text-2xl font-bold">TIMEFY</div>
+        <a href="./list" className="ml-auto cursor-pointer">
+          <div className="border-2 p-1 rounded-lg text-sm transition-all hover:bg-white hover:text-black">Open</div>
+        </a>
+      </nav>
+      <main className="p-3">Test</main>
+    </>
   )
 }
